0adc25a2ae72707623943511e4dd2f0e9a45404d
frechdachs285-sketch <frechdachs285@gmail.com>
2026-09-04 19:40:04 +0200

    Migrate settings storage to SharedPreferencesAsync

--- raetselkids/lib/services/settings_service.dart
+++ raetselkids/lib/services/settings_service.dart
@@ -4,23 +4,21 @@ class SettingsService {
   static const _speechKey = 'speech_enabled';
   static const _soundKey = 'sound_enabled';
 
+  final SharedPreferencesAsync _prefs = SharedPreferencesAsync();
+
   Future<bool> isSpeechEnabled() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_speechKey) ?? true;
+    return await _prefs.getBool(_speechKey) ?? true;
   }
 
   Future<void> setSpeechEnabled(bool enabled) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_speechKey, enabled);
+    await _prefs.setBool(_speechKey, enabled);
   }
 
   Future<bool> isSoundEnabled() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_soundKey) ?? true;
+    return await _prefs.getBool(_soundKey) ?? true;
   }
 
   Future<void> setSoundEnabled(bool enabled) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_soundKey, enabled);
+    await _prefs.setBool(_soundKey, enabled);
   }
 }
